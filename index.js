ad8f08b424dc3f7e10c6ff9c283ebbccc4c01775
Emir <emirmatik16@gmail.com>
2020-09-27 13:23:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.use('/users', users)
 app.use('/login', login)
 
 
-const port = process.env.PORT || 5000;
+let port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening from port: ${port}`))
